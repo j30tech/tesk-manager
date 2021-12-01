069ea5f11f9232715e44cdbba7bb2f06c249aa55
João Ribeiro <joao30tech@gmail.com>
2021-12-01 16:30:48 -0300

    - trabalahndo com array - lista de produtos

--- assets/src/main.js
+++ assets/src/main.js
@@ -1,12 +1,11 @@
-function  mostraNome (nome) {
-    console.log(`meu nome é: ${nome}`)
-}
-
-const item = ""
-const botao = document.querySelector("button")
-botao.addEventListener('click', function(event) {
-    const campoDeTexto = document.querySelector('input')
-    const lista = document.querySelector('ul')
-    const modelo = `<li>${campoDeTexto.value}</li>`
-    lista.insertAdjacentHTML('beforeend', modelo)
- })
+import { vitrine } from "./vitrine.js";
+const listaDeProdutos = [
+    {titulo: "produto 1", descricao: "descrição do produto 1"},
+    {titulo: "produto 2", descricao: "descrição do produto 2"},
+    {titulo: "produto 3", descricao: "descrição do produto 3"},
+    {titulo: "produto 4", descricao: "descrição do produto 4"},
+]
+const vitrineHTML = document.querySelector(".vitrine")
+const componenteVitrine = vitrine()
+const lista = componenteVitrine.modeloLista( componenteVitrine.modeloItem, listaDeProdutos )
+vitrineHTML.innerHTML = lista 
